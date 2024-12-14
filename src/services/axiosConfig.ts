5cde590b34be620a3dc3375c70d626e281e198e6
LangPN <langpn.dev@gmail.com>
2024-12-14 16:10:29 +0700

    update(endpoin): new API backend from ngrok

--- src/services/axiosConfig.ts
+++ src/services/axiosConfig.ts
@@ -3,7 +3,7 @@ import axios from 'axios'
 import Cookies from 'js-cookie'
 const api = axios.create({
   // baseURL: 'http://127.0.0.1:8000/api' // URL của API backend
-  baseURL: 'http://192.168.1.33:8000/api' // URL của API backend env
+  baseURL: 'https://4594-42-119-85-64.ngrok-free.app/api' // URL của API backend env
 })
 
 api.interceptors.request.use(
