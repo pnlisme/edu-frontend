4155be0696f318c5a8b64ac674768cdb65c902c8
LangPN <langpn.dev@gmail.com>
2024-12-15 00:11:10 +0700

    change(API): change endpoint to backend localhost

--- src/services/axiosConfig.ts
+++ src/services/axiosConfig.ts
@@ -3,7 +3,7 @@ import axios from 'axios'
 import Cookies from 'js-cookie'
 const api = axios.create({
   // baseURL: 'http://127.0.0.1:8000/api' // URL của API backend
-  baseURL: 'https://toannt.id.vn/public/api' // URL của API backend env
+  baseURL: 'http://127.0.0.1:8000/api' // URL của API backend env
 })
 
 api.interceptors.request.use(
